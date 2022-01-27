2a32b7cb19314e5d5785e409b6c817bc365283ea
uz <uz@trycatch.jp>
2022-01-27 12:19:54 +0900

    Improve tq command

--- cmd/tq/main.go
+++ cmd/tq/main.go
@@ -4,52 +4,132 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 
 	"github.com/jarxorg/tree"
+	"gopkg.in/yaml.v2"
 )
 
 const (
-	cmdName = "tq"
+	cmd          = "tq"
+	desc         = cmd + " is a portable command-line JSON/YAML processor."
+	usage        = cmd + " [flags] [query]"
+	examplesText = `Examples:
+  % echo '{"colors": ["red", "green", "blue"]}' | tq '.colors[0]'
+  "red"
+`
 )
 
+type format string
+
+func (f *format) String() string {
+	return string(*f)
+}
+
+func (f *format) Set(value string) error {
+	switch value {
+	case "json":
+		*f = "json"
+		return nil
+	case "yaml":
+		*f = "yaml"
+		return nil
+	}
+	return fmt.Errorf("unknown format")
+}
+
 var (
-	isHelp = flag.Bool("help", false, "help for "+cmdName)
+	inputFormat  = format("json")
+	outputFormat = format("json")
 )
+
+func init() {
+	flag.Bool("h", false, "help for "+cmd)
+	flag.Var(&inputFormat, "i", `input format (json or yaml) (default "json")`)
+	flag.Var(&outputFormat, "o", `output format (json or yaml) (default "json")`)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s\n\nUsage:\n  %s\n\n", desc, usage)
+		fmt.Fprintln(os.Stderr, "Flags:")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\n%s", examplesText)
+	}
+}
 
 func main() {
 	flag.Parse()
+	handleError(run())
+}
 
-	if *isHelp {
-		flag.Usage()
-		return
-	}
-
-	if err := exec(); err != nil {
+func handleError(err error) {
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
 	}
 }
 
-func exec() error {
-	in, err := ioutil.ReadAll(os.Stdin)
+func run() error {
+	switch inputFormat {
+	case "yaml":
+		return runYAML()
+	}
+	return runJSON()
+}
+
+func runJSON() error {
+	i := 0
+	dec := json.NewDecoder(os.Stdin)
+	for dec.More() {
+		var v interface{}
+		if err := dec.Decode(&v); err != nil {
+			return err
+		}
+		if err := evaluate(tree.ToNode(v), i); err != nil {
+			return err
+		}
+		i++
+	}
+	return nil
+}
+
+func runYAML() error {
+	i := 0
+	dec := yaml.NewDecoder(os.Stdin)
+	for {
+		var v interface{}
+		if err := dec.Decode(&v); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if err := evaluate(tree.ToNode(v), i); err != nil {
+			return err
+		}
+		i++
+	}
+	return nil
+}
+
+func evaluate(node tree.Node, i int) error {
+	node, err := tree.Find(node, flag.Arg(0))
 	if err != nil {
 		return err
 	}
-	node, err := tree.UnmarshalJSON(in)
-	if err != nil {
-		return err
+	switch outputFormat {
+	case "yaml":
+		out, err := tree.MarshalYAML(node)
+		if err != nil {
+			return err
+		}
+		if i > 0 {
+			fmt.Println("---")
+		}
+		fmt.Print(string(out))
+		return nil
 	}
-	query, err := tree.ParseQuery(flag.Arg(0))
-	if err != nil {
-		return err
-	}
-	result, err := query.Exec(node)
-	if err != nil {
-		return err
-	}
-	out, err := json.MarshalIndent(result, "", "  ")
+	out, err := json.MarshalIndent(node, "", "  ")
 	if err != nil {
 		return err
 	}
